0148d31c0dce0a8827c256165c7bafc7a6acf2fd
prathamesh-p11 <prathameshp2472@gmail.com>
2020-06-06 13:59:02 -0700

    Compare player guess and loop till correct answer || lives = 0

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -3,10 +3,59 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
-    Super::BeginPlay();
+    Super::BeginPlay(); 
+    SetupGame(); //initializing game
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
 
+    if (bGameOver)
+    {
+        ClearScreen();
+        SetupGame();
+    }
+    else
+    {
+        if (Input == HiddenWord)
+        {
+            PrintLine(TEXT("You Won!"));
+            EndGame();
+        }
+
+        else
+        {
+            --Lives;
+            if(Lives > 0)
+            {
+                if (HiddenWord.Len() != Input.Len())
+                {
+                    PrintLine(TEXT("Hidden word length is eual to %i, try again!!"), HiddenWord.Len());
+                }
+            }
+            else
+            {
+                PrintLine(TEXT("You have no lives left!!"));
+            }
+        }
+    }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("hour");
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+
+    PrintLine(TEXT("Welcome to bull cow game!"));
+    PrintLine(TEXT("Total lives remaining: %i"), Lives);
+    PrintLine(TEXT("Guess the %i letter word:"), HiddenWord.Len());
+
+    PrintLine(TEXT("Press enter to continue..."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press Enter to play again...."));
 }
